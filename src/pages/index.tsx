c2a57bd3780f67ea6772555a257f76cb2988bbc4
Amritansh Pandey <amritansh.pandey6@gmail.com>
2025-07-16 22:45:15 +0530

    inisgts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to my Portfolio</h1>
+      <h1>Welcome to my Poccxtfolio</h1>
       <p>This is a placeholder for the Home page.</p>
     </div>
   );

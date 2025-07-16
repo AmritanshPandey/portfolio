10df1a59679d4765f4b6ef986dbe94b167a08ca3
Amritansh Pandey <amritansh.pandey6@gmail.com>
2025-07-16 22:54:54 +0530

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import Layout from '@/components/global/Layout';
 import type { AppProps } from 'next/app';
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </Layout>
       <SpeedInsights />
+      <Analytics />
     </main>
   );
 }

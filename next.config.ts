97594e2394ef9a330de8f738fded2fb1957d09e4
Amritansh Pandey <amritansh.pandey6@gmail.com>
2025-07-16 22:28:16 +0530

    inisgts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: 'out',
-  trailingSlash: true
+
 };
 
 export default nextConfig;
